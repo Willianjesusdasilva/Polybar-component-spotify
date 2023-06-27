4ecce79698eb1e8d2369d4e873b751794ad5a203
Willian Jesus da Silva <46569232+Willianjesusdasilva@users.noreply.github.com>
2023-06-27 12:37:47 -0300

    Create scroll_spotify_status.sh

---
+++ scripts/scroll_spotify_status.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+zscroll -l 30 \
+        --delay 0.1 \
+        --scroll-padding "  " \
+        --match-command "`dirname $0`/get_spotify_status.sh --status" \
+        --match-text "Playing" "--scroll 1" \
+        --match-text "Paused" "--scroll 1" \
+        --update-check true "`dirname $0`/get_spotify_status.sh" &
+
+wait
+
